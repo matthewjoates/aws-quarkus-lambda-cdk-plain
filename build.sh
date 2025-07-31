e2fe115fdc35148637ba58cf36db35a900e2b460
matthewjoates <>
2025-07-31 19:33:04 +0100

    clean

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-echo "building functions"
-cd lambda && mvn clean package
-echo "building lambda-st"
-cd ../lambda-st && mvn clean package
-echo "building CDK"
-cd ../cdk && mvn clean package
--- buildAndDeploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -e
-echo "building functions"
-cd lambda && mvn clean package
-echo "building CDK"
-cd ../cdk && mvn clean package && cdk deploy
