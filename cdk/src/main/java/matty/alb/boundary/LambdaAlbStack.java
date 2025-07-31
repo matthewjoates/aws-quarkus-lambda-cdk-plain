1ecb7c1b582a477620ccee093cf579bc63250c12
matthewjoates <>
2025-07-31 18:54:28 +0100

    initial commit

---
+++ cdk/src/main/java/matty/alb/boundary/LambdaAlbStack.java
@@ -0,0 +1,50 @@
+package matty.alb.boundary;
+
+import java.util.List;
+import java.util.Map;
+
+import matty.alb.control.Alb;
+import matty.alb.control.PublicVPC;
+import matty.lambda.control.QuarkusLambda;
+import software.amazon.awscdk.CfnOutput;
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.services.elasticloadbalancingv2.AddApplicationTargetsProps;
+import software.amazon.awscdk.services.elasticloadbalancingv2.BaseApplicationListenerProps;
+import software.amazon.awscdk.services.elasticloadbalancingv2.HealthCheck;
+import software.amazon.awscdk.services.elasticloadbalancingv2.targets.LambdaTarget;
+import software.constructs.Construct;
+
+public class LambdaAlbStack extends Stack {
+
+    static String FUNCTION_NAME  = "matty_lambda_gretings_boundary_Greetings";
+
+    public LambdaAlbStack(Construct scope, String id) {
+        super(scope, id+"-alb-stack");
+        var configuration = Map.of(
+            "message", "hello, quarkus as AWS Lambda");
+
+        var quarkuLambda = new QuarkusLambda(this,FUNCTION_NAME,configuration);
+        var publicVPCConstruct = new PublicVPC(this);
+        var publicVPC = publicVPCConstruct.getVpc();
+        var alb = new Alb(this, publicVPC, "MattyLambdaLB");
+
+        var function = quarkuLambda.getFunction();
+        var lambdaTarget = new LambdaTarget(function);
+        
+        var loadBalancer = alb.getApplicationLoadBalancer();
+        var listener = loadBalancer.addListener("Http", BaseApplicationListenerProps.builder()
+                                                                            .port(80)
+                                                                            .build()); 
+                                                                            
+        listener.addTargets("Lambda", AddApplicationTargetsProps.builder()
+                                        .targets(List.of(lambdaTarget))
+                                        .healthCheck(HealthCheck.builder()
+                                                        .enabled(true)
+                                                        .build())
+                                        .build());    
+        CfnOutput.Builder.create(this, "FunctionARN").value(function.getFunctionArn()).build();
+        var url = loadBalancer.getLoadBalancerDnsName();
+        CfnOutput.Builder.create(this, "LoadBalancerDNSName").value(url).build();
+        CfnOutput.Builder.create(this, "LoadBalancerCurlOutput").value("curl -i http://"+url+"/hello").build();
+    }
+}
---
+++ cdk/src/test/java/matty/lambda/control/QuarkusLambdaTest.java
@@ -0,0 +1,22 @@
+package matty.lambda.control;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+
+import matty.lambda.control.QuarkusLambda;
+ 
+public class QuarkusLambdaTest {
+
+
+    @Test
+    public void testMergeWithRuntimeConfiguration() {
+        var actual = QuarkusLambda.mergeWithRuntimeConfiguration(Map.of("msg","duke"));
+        assertThat(actual).containsKey("JAVA_TOOL_OPTIONS");
+        assertThat(actual).containsEntry("msg", "duke");
+        
+    }
+
+
+}
